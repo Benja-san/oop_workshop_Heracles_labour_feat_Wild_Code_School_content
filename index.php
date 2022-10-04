861d58f180db96080fd8656f345fd619c983b40b
benja-san <beugnet.freelance@gmail.com>
2022-10-04 15:07:37 +0200

    Alone commit for Workshop

--- index.php
+++ index.php
@@ -1,4 +1,84 @@
 <?php
-
+//Require all classes I need
+require_once "src/Fighter.php";
 // First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+// use your Fighter class here
+$heracles = new Fighter("Heracles", 6, 20);
+$LionOfNemee = new Fighter("Lion de Nemée", 13, 11);
+$roundNumber = 1;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heracles Labour</title>
+</head>
+<body>
+    <main>
+        <h1>Heracles Labour Part 1</h1>
+        <section>
+            <div>
+                <h2><?= $heracles->getName() ?></h2>
+                <p>Strength : <?= $heracles->getStrength() ?></p>
+                <p>Dexterity : <?= $heracles->getDexterity() ?></p>
+                <p>Life points : <?= $heracles->getLife() ?></p>
+            </div>
+
+            <div>
+                <h2><?= $LionOfNemee->getName() ?></h2>
+                <p>Strength : <?= $LionOfNemee->getStrength() ?></p>
+                <p>Dexterity : <?= $LionOfNemee->getDexterity() ?></p>
+                <p>Life points : <?= $LionOfNemee->getLife() ?></p>
+            </div>
+        </section>
+        <section>
+            <h2>Fight mode :</h2>
+            <?php while($heracles->getLife() > 0 && $LionOfNemee->getLife() > 0 ) : ?>
+                <h3>Round number : <?= $roundNumber ?></h3>
+                <p>
+                    <?= $heracles->getName() ?> 
+                    Attacks 
+                    <?= $LionOfNemee->getName() ?> 
+                    and inflicts 
+                    <?= $heracles->fight($LionOfNemee) ?>  
+                    of damage
+                </p>
+
+                <?php if($LionOfNemee->getLife() > 0) : ?>
+                    <p>
+                        <?= $LionOfNemee->getName() ?> 
+                        Attacks 
+                        <?= $heracles->getName() ?> 
+                        and inflicts 
+                        <?= $LionOfNemee->fight($heracles) ?>  
+                        of damage
+                    </p>
+                <?php endif ?>
+
+                <p>
+                    <?= $LionOfNemee->getName() ?> has now <?= $LionOfNemee->getLife() ?> Life points.
+                </p>
+                <p>
+                    <?= $heracles->getName() ?> has now <?= $heracles->getLife() ?> Life points.
+                </p>
+                <?php $roundNumber++ ?>
+            <?php endwhile ?>
+        </section>
+        <section>
+            <h3>
+                <?php if ($heracles->getLife() === 0) : ?>
+                    <?php if($LionOfNemee->getLife() === 0) : ?>
+                        "Draw"
+                    <?php endif ?>
+                    <?= $LionOfNemee->getName() . "Won the fight!" ?>
+                <?php else : ?>
+                    <?= $heracles->getName() . "Won the fight!" ?>
+                <?php endif ?>
+            </h3>
+        </section>
+    </main>
+</body>
+</html>
--- src/Fighter.php
+++ src/Fighter.php
@@ -1,3 +1,70 @@
 <?php
 
 /* Fighter class definition */
+class Fighter
+{
+
+    //Constants
+    public const MAX_LIFE = 100;
+
+    //Properties
+    private string $name;
+    private int $dexterity;
+    private int $strength;
+    private int $life = self::MAX_LIFE;
+
+    //Magic methods
+    public function __construct(string $name, int $dexterity, int $strength){
+        $this->name = $name;
+        $this->dexterity = $dexterity;
+        $this->strength = $strength;
+    }
+
+    //Getter and Setter Methods
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getDexterity(): int
+    {
+        return $this->dexterity;
+    }
+
+    public function setDexterity(int $dexterity): void
+    {
+        $this->dexterity = $dexterity;
+    }
+
+    public function getStrength(): int
+    {
+        return $this->strength;
+    }
+
+    public function setStrength(int $strength): void
+    {
+        $this->strength = $strength;
+    }
+
+    public function getLife(): int
+    {
+        return $this->life;
+    }
+
+    public function setLife(int $life): void
+    {
+        $this->life = $life;
+    }
+
+    //Logical methods
+    public function fight(Fighter $defender) : int
+    {
+        $attackPower = rand(1, $this->strength);
+        $defenderDamage = $attackPower - $defender->getDexterity();
+        $inflictedDamages = $defenderDamage <= 0 ? 0 : $defenderDamage;
+        $defenderLifePoints = $defender->getLife();
+        $lifeRemaining = ($defenderLifePoints - $inflictedDamages) <= 0 ? 0 : $defenderLifePoints - $inflictedDamages;
+        $defender->setLife($lifeRemaining);
+        return $inflictedDamages;
+    }
+}
